a9be37e62d35c155e5c514fd5e766ad99f9ef408
jason-albers <jalbers@gunnauto.com>
2025-06-20 18:04:43 -0500

    project finished. it's a mess. do not look at it.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Hero = () => {
   return (
     <Wrapper>
-      <HeroImage src="/images/hero-img.jpg" />
-      <Swoop src="/swoop.svg" />
+      <picture>
+        <source
+          type="image/avif"
+          srcSet="
+      /images/hero-img.avif 1x,
+      /images/hero-img@2x.avif 2x,
+      /images/hero-img@3x.avif 3x
+    "
+        />
+        <source
+          type="image/jpg"
+          srcSet="
+          /images/hero-img.jpg 1x,
+          /images/hero-img@2x.jpg 2x,
+          /images/hero-img@3x.jpg 3x
+    "
+        />
+        <HeroImage
+          src="/images/hero-img.jpg"
+          alt="Hero banner, a majestic tabby cat looking forward."
+        />
+      </picture>
+      <Swoop src="/swoop.svg" alt="" />
     </Wrapper>
   );
 };
--- src/components/PhotoGridItem/PhotoGridItem.jsx
+++ src/components/PhotoGridItem/PhotoGridItem.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const PhotoGridItem = ({ id, src, alt, tags }) => {
-    function getSources(src, extension) {
+  function getSources(src, extension) {
     const regular = src.replace(".jpg", `.${extension} 1x`);
     const at2x = src.replace(".jpg", `@2x.${extension} 2x`);
     const at3x = src.replace(".jpg", `@3x.${extension} 3x`);
@@ -17,7 +17,7 @@ const PhotoGridItem = ({ id, src, alt, tags }) => {
         <picture>
           <source type="image/avif" srcSet={getSources(src, "avif")} />
           <source type="image/jpg" srcSet={getSources(src, "jpg")} />
-          <Image src={src} alt={alt=''}/>
+          <Image src={src} alt={alt} />
         </picture>
       </Anchor>
       <Tags>
@@ -45,17 +45,26 @@ const Image = styled.img`
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
+  display: block;
+  max-width: 100%;
+  white-space: nowrap;
+
+  &:last-child {
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
 `;
 
 const Tag = styled.li`
+  display: inline;
   padding: 4px 8px;
+  margin-right: 8px;
   background: var(--color-gray-300);
   font-size: 0.875rem;
   font-weight: 475;
+  line-height: 1.6;
   color: var(--color-gray-800);
+  max-width: fit-content;
 `;
 
 export default PhotoGridItem;
